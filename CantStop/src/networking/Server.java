d6fcc00a20fab08d7a319176ed90d17d860164a0
Matthew <kovelov@gmail.com>
2013-06-11 17:09:34 -0300

    Login File IO mostly done, able to add new user info, able to read lines in the loginInfo file "CantStop.txt". Need to add the cases for looking for existing user.

--- CantStop/src/networking/Server.java
+++ CantStop/src/networking/Server.java
@@ -1,6 +1,7 @@
 package networking;
 import java.io.*;
 import java.net.*;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Server {
@@ -11,27 +12,124 @@ public class Server {
 	BufferedReader in = null;
     PrintWriter out = null;
     BufferedReader fileReader = null;
-    PrintWriter fileWriter = null;
     static int numConnected=0;
+    String loginInfo = "CantStop.txt";
+    Scanner loginSc = null;
 	
 	//Initialize Server and ServerSocket
 	public Server()	{
 		portNumber=2043;
-		try{
+		try
+		{
+			File loginFile = new File(loginInfo);
+			if(loginFile.createNewFile())
+			{
+				loginFile.createNewFile();
+				System.out.println("File Created: "+loginFile.getName());//debug
+				System.out.println("File Path: "+loginFile.getAbsolutePath());//debug
+			}
+			else
+			{
+				System.out.println("File already exists.");//debug
+			}//debug
+		}
+		catch(IOException e)
+		{
+			System.err.println("Could not handle file: "+loginInfo+": " + e.getMessage());
+		}
+		try
+		{
 			s=new ServerSocket(portNumber);
-			//initFileIO();//set up the file reader and writer for login.
+			System.out.println("Server started on port: "+portNumber);//debug
+			addNewUser("test,test");
+			checkExistingUser("test,test");
 		}//try
 		catch(IOException e){
 			System.err.println("Could not listen on port: "+portNumber+". " + e.getMessage());
 	        System.exit(-1);
 		}//catch
 	}//Method
+	private void checkExistingUser(String requested)
+	{
+		boolean existing=false;
+		try
+		{
+			System.out.println("Login File reader to be created: "+loginInfo);//debug
+			fileReader = new BufferedReader(new FileReader(loginInfo));
+			loginSc = new Scanner(fileReader);
+			String line = loginSc.nextLine();
+			System.out.println(line);//debug
+			boolean scan = true;
+			while(scan)
+			{
+				if(line.equals(requested))
+				{
+					existing=true;
+				}
+				System.out.println(line);//debug
+				System.out.println("Matches requested: "+existing);//debug
+				try
+				{
+					line=loginSc.nextLine();
+				} catch (NoSuchElementException e) {
+					System.err.println("Could not use Scanner. " + e.getMessage());
+					scan=false;
+				}
+			}
+		}
+		catch(IOException e)
+		{
+			System.err.println("Could not use File Reader or Scanner" + e.getMessage());
+	        System.exit(-1);
+		}
+		finally
+		{
+			loginSc.close();
+			try 
+			{
+				fileReader.close();
+			} catch (IOException e) 
+			{
+				System.err.println("Could not close File Reader or Scanner" + e.getMessage());
+			}
+		}
+		if(existing)
+		{
+			//allowed
+			System.out.println("Allowed to connect.");//debug
+		}
+		else
+		{
+			//Rejected, reply to client with error from CS protocol.
+			System.out.println("err, User does not exist.");//debug
+		}
+	}//Method - checkExistingUser
+	private void addNewUser(String write)
+	{
+		try
+		{
+			System.out.println("Adding new User Login.");//debug
+			FileWriter fileWriter = new FileWriter(loginInfo,true);
+			System.out.println("Should write: " + write);
+			fileWriter.write(write+System.getProperty("line.separator"));
+			
+			fileWriter.flush();
+			fileWriter.close();
+			System.out.println("Finished adding new User Login.");//debug
+		}
+		catch(IOException e)
+		{
+			System.err.println("Could not add user to records." + e.getMessage());
+	        System.exit(-1);
+		}
+	}
 	public void initFileIO()
 	{
 		try
 		{
-			fileReader = new BufferedReader(new FileReader("CantStop.txt"));
-			fileWriter = new PrintWriter(new FileWriter("CantStop"));
+			System.out.println("Login File reader to be created: "+loginInfo);//debug
+			fileReader = new BufferedReader(new FileReader(loginInfo));
+			loginSc = new Scanner(fileReader);
 		}
 		catch(IOException e)
 		
