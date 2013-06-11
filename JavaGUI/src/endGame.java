43a30047218f3bed3c1cbf7ddc96626227e5102a
Kelsey LaPointe <i4v5p@unb.ca>
2013-06-11 16:21:00 -0300

    updates to the GUI

---
+++ JavaGUI/src/endGame.java
@@ -0,0 +1,51 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.FlowLayout;
+
+
+public class endGame extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					endGame frame = new endGame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public endGame() {
+		setTitle("Can't Stop!");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 575, 449);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(255, 255, 102));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		
+		JLabel lblCongratulations = new JLabel("CONGRATULATIONS!");
+		lblCongratulations.setFont(new Font("Orator Std", Font.PLAIN, 23));
+		contentPane.add(lblCongratulations);
+	}
+
+}
